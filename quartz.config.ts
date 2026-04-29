7e15a716b200ebf3164af84dc18dbbc1a023db13
cleanthelawschool <cleanthelawschool@gmail.com>
2026-04-29 18:01:22 +0900

    Initial Quartz Upload

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cleanthelawschool.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
